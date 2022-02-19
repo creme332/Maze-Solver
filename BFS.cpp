0f482e871d7154a7f16ca39d0e9898c122eab110
creme332 <65414576+creme332@users.noreply.github.com>
2022-02-19 07:49:58 +0400

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -4,8 +4,9 @@
 #include <deque>
 
 using namespace std;
-map <pair<int, int>, bool> explored; //stores coordinates already explored
-//maze must be a n x n square. 
+
+//Goal : Reach bottom right corner of maze from (0,0). Can move in any orthogonal direction.
+
 vector <vector<char>> m = {
     {'.','W','.','.','.','W','.','.','.'},
     {'.','.','.','W','.','W','.','W','.'},
@@ -17,12 +18,9 @@ vector <vector<char>> m = {
     {'W','.','.','.','.','W','.','.','.'},
     {'.','.','.','W','.','.','.','W','.'},
 
-
-
 };
-deque <pair<int, int>> shortestpath; //stores nodes along shortest path
 
-//Goal : Reach (n-1,n-1) from (0,0). Can move in any orthogonal direction
+deque <pair<int, int>> shortestpath; //stores nodes along shortest path
 
 string color(string a) { //color character a 
     if (a == "o")return "\x1B[33m0\033[0m"; //yellow character and trail
@@ -30,25 +28,12 @@ string color(string a) { //color character a
     if (a == "W")return "\x1B[31mW\033[0m"; //red walls
     return "\x1B[92m" + a + "\033[0m"; //cyan ground and finish point
 }
-void output(int r, int c) {
-    //(r,c) current position
-    for (int k = 0;k < 10000000;k++);
-    system("CLS");
 
-    for (int i = 0;i < m.size();i++) {
-        for (int j = 0;j < m[i].size();j++) { 
-            if (i == r && j == c) { cout << color("o") << "   "; }
-            else { string c = ""; c += m[i][j]; cout << color(c) << "   "; }
-        }
-        cout << "\n";
-        cout << "\n";
-    }
-}
 void output1() { 
     //animate shortest path for BFS()
     //move character + leave a trail behind.
  
-    for (auto n : shortestpath) { //for each node along shortest path
+    for (auto n : shortestpath) { //output each node along shortest path
 
         for (int i = 0;i < m.size();i++) {
             for (int j = 0;j < m[i].size();j++) {
@@ -74,27 +59,7 @@ void output1() {
         system("CLS");
 
     }
-
 }
-
-bool DFS(int row, int col) { //called as DFS(0,0)
-    output(row, col); //output current position
-    vector <int> dr = { 1,0,-1,0 }; // translation horizontally
-    vector <int> dc = { 0,1,0,-1 }; //translation vertically
-
-    //if destination reached, return 1
-    if (row == m.size() - 1 && col == m[row].size() - 1) { return 1; }
-    int rr, cc;
-    for (int i = 0;i < 4;i++) {
-        rr = row + dr[i]; cc = col + dc[i];
-        if (rr > -1 && rr <m.size() && cc>-1 && cc < m[m.size() - 1].size() && m[rr][cc] != 'W' && explored[{rr, cc}] == 0) {
-            explored[{row, col}] = 1; //update list of explored coordinates
-            if (path(rr, cc) == 1)return 1;
-        }
-    }
-    return 0;
-}
-
 
 int BFS() {// find shortest path using BFS
     map <pair<int, int>, int> distance = { {{0, 0},0} }; //minimum distance from start (in terms of number of moves required)
@@ -140,12 +105,8 @@ int BFS() {// find shortest path using BFS
     return 0;
 }
 
-
 int main() {
-    cout << BFS() << endl; //start at (0,0) and move to (n-1, n-1)
-    output1(); //NOTE : maze is permanently modified after this call and cannot be used again.
-    
-    //cout<<DFS();
-
+    cout << BFS() << endl; 
+    output1(); //NOTE : maze is permanently modified after this call and cannot be used again.    
 
 }
